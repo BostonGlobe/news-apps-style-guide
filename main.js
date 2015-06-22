53668e448ac88610eb41ef63b08da50f498c49b2
russell goldenberg <russellgoldenberg@gmail.com>
2015-06-22 13:53:13 -0400

    tabs

--- main.js
+++ main.js
@@ -8,6 +8,15 @@
 			var $insert = $(_pre);
 			
 			var html = $el.html();
+
+			// hack to reformat properly in prism without sacrificing html dev
+			html = html.replace(/\n/g, '*n*');
+			html = html.replace(/\s{4}/g, '*t*');
+			html = html.replace(/\*n\*/g, '\n');
+			html = html.replace(/(\*t\*){4}/g, '');
+			html = html.replace(/\*t\*/g, '    ');
+			html = html.trim();
+
 			$insert.find('code').text(html);
 			$insert.insertAfter($el);
 		});
